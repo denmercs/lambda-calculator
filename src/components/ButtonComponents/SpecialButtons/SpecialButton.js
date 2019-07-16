32a681e4c4a9a756a517ae78dde5ee6ad0086d66
denmercs <denmercs@gmail.com>
2019-07-16 18:21:52 -0500

    done with the MVP

--- src/components/ButtonComponents/SpecialButtons/SpecialButton.js
+++ src/components/ButtonComponents/SpecialButtons/SpecialButton.js
@@ -4,7 +4,7 @@ const SpecialButton = props => {
   return (
     <div>
       {/* Display a button element rendering the data being passed down from the parent container on props */}
-      <button className="specialBtn">{props.specialsMapped}</button>
+      <button className="specialBtn">{props.specialMap}</button>
     </div>
   );
 };
