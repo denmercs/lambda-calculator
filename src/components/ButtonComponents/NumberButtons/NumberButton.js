bfb80f06276c331efa4d4d95cc0d75c31fa25c20
denmercs <denmercs@gmail.com>
2019-07-16 18:08:39 -0500

    done with the 3 components with no errors. working on display

--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -1,9 +1,13 @@
 import React from "react";
+import { tsPropertySignature } from "@babel/types";
 
-const NumberButton = () => {
+const NumberButton = props => {
   return (
-    <>
+    <div>
       {/* Display a button element rendering the data being passed down from the parent container on props */}
-    </>
+      <button className="numberBtn">{props.numbersMap}</button>
+    </div>
   );
 };
+
+export default NumberButton;
--- src/components/DisplayComponents/Display.js
+++ src/components/DisplayComponents/Display.js
@@ -1,5 +1,28 @@
 import React from "react";
+import Numbers from "../ButtonComponents/NumberButtons/Numbers";
+import Operators from "../ButtonComponents/OperatorButtons/Operators";
+import Specials from "../ButtonComponents/SpecialButtons/Specials";
+import Logo from "../DisplayComponents/Logo";
 
 const Display = () => {
-  return <div>{/* Display any props data here */}</div>;
+  return (
+    <div className="calculatorContainer">
+      {/* Display any props data here */}
+      <div className="calculatorLogo">
+        <Logo />
+      </div>
+
+      <div className="calculatorButtons">
+        <div>
+          <Specials />
+          <Numbers className="numbersBtn" />
+        </div>
+        <div>
+          <Operators />
+        </div>
+      </div>
+    </div>
+  );
 };
+
+export default Display;
